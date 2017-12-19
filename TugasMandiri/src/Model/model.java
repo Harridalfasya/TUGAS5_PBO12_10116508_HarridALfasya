0cc550292abb56ea3379dfbbeb8b8f5ba3ee64e2
Harridalfasya <harrid.alfasya@gmail.com>
2017-12-20 03:42:50 +0700

    Add files via upload

---
+++ TugasMandiri/src/Model/model.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Harrid Alfasya
+ */
+public class model {
+        String nama, username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+        
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+        
+    }
